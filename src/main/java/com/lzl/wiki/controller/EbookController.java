8886ce631094ce32104b4ee9799830d049f12b11
lzlonng <1661548820@qq.com>
2022-01-08 21:54:03 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/lzl/wiki/controller/EbookController.java
@@ -0,0 +1,35 @@
+package com.lzl.wiki.controller;
+
+import com.lzl.wiki.domain.Ebook;
+import com.lzl.wiki.service.impl.EbookServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <h3>wiki</h3>
+ * <p>HelloWorld</p>
+ *
+ * @author : 黎钟龙
+ * QQ:1661548820
+ * Mail：1661548820@qq.com
+ * @date : 2022-01-08 08:37
+ **/
+//返回的是一个字符串
+//@Controller
+//    返回json格式字符串
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+//    引入service
+    @Resource
+    private EbookServiceImpl ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/lzl/wiki/service/EbookService.java
@@ -0,0 +1,17 @@
+package com.lzl.wiki.service;
+
+import com.lzl.wiki.domain.Ebook;
+
+import java.util.List;
+
+/**
+ * <h3>wiki</h3>
+ * <p>测试类的Service</p>
+ *
+ * @author : 黎钟龙
+ * QQ:1661548820
+ * Mail：1661548820@qq.com
+ **/
+public interface EbookService {
+    public List<Ebook> list();
+}
