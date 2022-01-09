531c07b8b444406f02403280a0bfc29948180306
lzlonng <1661548820@qq.com>
2022-01-09 17:22:53 +0800

    4-6 网站首页布局开发

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,10 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//引入组件
 import Antd from 'ant-design-vue';
-//引入css
 import 'ant-design-vue/dist/antd.css';
-
-//使用一下
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+createApp(App).use(Antd).use(store).use(router).mount('#app')
