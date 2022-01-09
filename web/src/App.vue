30620e28e5e7991c5101e33674859c5767e9a29e
lzlonng <1661548820@qq.com>
2022-01-09 18:11:09 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,14 +7,8 @@
 <!--      变化的部分动态链接-->
       <router-view/>
 <!--    公共的尾部-->
-    <a-layout-footer style="text-align: center">
-      第一个实战项目Wiki电子书
-      <br>
-      <hr>
-<!--      <img alt="备案图标" src="webicon.png">-->
-      备案号：
-      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=cdblog.live">黔ICP备2021010120号-2</a>
-    </a-layout-footer>
+<!--    引入尾部组件-->
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
@@ -22,12 +16,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 //导入组件不能用横线要用驼峰式命名
+//头部组件
 import TheHeader from '@/components/the-header.vue';
+//尾部组件
+import TheFooter from '@/components/the-footer.vue';
 export default defineComponent({
   //组件名字
   name: 'app',
   components:{
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 });
 </script>
