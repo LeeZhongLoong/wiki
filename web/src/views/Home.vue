b0584d0240f9ec51c57498f53e7add0fbf5fdac1
lzlonng <1661548820@qq.com>
2022-01-09 20:51:35 +0800

    5-4 电子书布局调整 一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -47,35 +47,30 @@
     </a-layout-sider>
 <!--    页面左边内容-->
     <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}">
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <template #footer>
-          <div>
-            <b>ant design vue</b>
-            footer part
-          </div>
-        </template>
+<!--      让他并排显示 column:3一行变为三列,gutter20每列间距为20-->
+      <a-list item-layout="vertical" size="large" :grid="{ gutter:20,column:3}" :pagination="pagination" :data-source="ebooks">
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+          <a-list-item key="item.name">
             <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
-            <template #extra>
-              <img
-                  width="272"
-                  alt="logo"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            </template>
+<!--            <template #extra>-->
+<!--              <img-->
+<!--                  width="272"-->
+<!--                  alt="logo"-->
+<!--                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"-->
+<!--              />-->
+<!--            </template>-->
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.cover }}</a>
               </template>
               <template #avatar><a-avatar :src="item.avatar" /></template>
             </a-list-item-meta>
-            {{ item.content }}
+<!--            {{ item.content }}-->
           </a-list-item>
         </template>
       </a-list>
