6bf9571f6e48dbe775883ab8f19ff43ccc357216
lzlonng <1661548820@qq.com>
2022-01-09 17:57:19 +0800

    4-7 制作the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,19 +1,8 @@
 <template>
   <a-layout>
 <!--    公共的头部-->
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+<!--    使用头部组件-->
+    <the-header></the-header>
 <!--    公共动态链接-->
 <!--      变化的部分动态链接-->
       <router-view/>
@@ -28,6 +17,20 @@
     </a-layout-footer>
   </a-layout>
 </template>
+
+<!--导入组件-->
+<script lang="ts">
+import { defineComponent } from 'vue';
+//导入组件不能用横线要用驼峰式命名
+import TheHeader from '@/components/the-header.vue';
+export default defineComponent({
+  //组件名字
+  name: 'app',
+  components:{
+    TheHeader
+  }
+});
+</script>
 
 <style>
 #components-layout-demo-top-side-2 .logo {
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,27 @@
+<!--提取头部组件-->
+<template>
+  <!--    公共的头部-->
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 111</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<!--定义的组件-->
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  //组件名字
+  name: 'the-header'
+});
+</script>
