9190ba2f25e827ee0a0956857867456d8106bf1f
lzlonng <1661548820@qq.com>
2022-01-09 18:11:09 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,21 @@
+<template>
+<!--  设置尾部组件-->
+  <!--    公共的尾部-->
+  <a-layout-footer style="text-align: center">
+    第一个实战项目Wiki电子书
+    <br>
+    <hr>
+    备案号：
+    <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=cdblog.live">黔ICP备2021010120号-2</a>
+  </a-layout-footer>
+</template>
+
+<!--设置组件-->
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+//  文件名字
+  name:'the-footer'
+});
+</script>
