6296291fbe48679e5248621837ce5c3969122719
lzlonng <1661548820@qq.com>
2022-02-08 09:01:56 +0800

    8-12 使用树形组件展示文档树 遗留BUG，文档树应该只查当前电子书的文档，而不是全部文档

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -1,11 +1,98 @@
 <template>
   <a-layout>
-    <a-laout-content :style="{background:'#fff',padding:'24px',margin:0,minHeight:'280px'}">
-      <div>
-        <div class="doc">
-          <h1>欢迎来到文档页面</h1>
-        </div>
-      </div>
+    <a-laout-content  :style="{background:'#fff',padding:'24px',margin:0,minHeight:'280px'}">
+      <a-row>
+        <a-col :span="6">
+          <a-tree
+            v-if="level1.length>0"
+            :tree-data="level1"
+            @select="onselect"
+            :replaceFields="{title:'name',key:'id',value:'id'}"
+            :defaultExpandAll="true"
+          >
+
+          </a-tree>
+        </a-col>
+        <a-col :span="18">
+        </a-col>
+      </a-row>
     </a-laout-content>
   </a-layout>
 </template>
+
+<script lang="ts">
+
+//导入
+import {defineComponent, onMounted, ref} from 'vue';
+//导入axios
+import axios from 'axios';
+//导入消息组件
+import {message} from "ant-design-vue";
+import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
+
+
+
+export default defineComponent({
+
+  name:'Doc',
+  setup() {
+    //路由
+    const route=useRoute();
+    //后端传过来的数据
+    const docs=ref();
+    //等待框的初始值
+    const loading=ref(false);
+
+    //  数组树
+    /**
+     * 一级文档，children属性就是二级文档
+     * [{
+     *    id:"",
+     *    name:"",
+     *    children:[{
+     *      id:"",
+     *      name:"",
+     *      }]
+     *  }]
+     */
+        //一级文档树，children是二级树
+    const level1=ref();
+    level1.value=[];
+    //  数据查询
+    const handleQuery=()=>{
+      //让查询之前有数据等待样式
+      loading.value=true;
+      axios.get("/doc/all/"+route.query.ebookId).then((response)=>{
+        loading.value=false;
+        const data=response.data;
+        if (data.success){
+          docs.value=data.content;
+          console.log("原始数组:",docs.value);
+
+          //level1定义为数组
+          level1.value=[];
+          level1.value=Tool.array2Tree(docs.value,0);
+        }else{
+          message.error(data.message);
+        }
+      })
+    }
+
+
+    //初始的方法
+    onMounted(function (){
+      //只在方法内调用
+      handleQuery();
+    });
+    //返回所有的参数
+    return {
+      //返回数组树
+      level1,
+      loading
+    }
+  },
+});
+
+</script>
+
