d2e8af10b82dbdce8350f70eebffb0a61e110705
lzlonng <1661548820@qq.com>
2022-02-09 16:57:09 +0800

    9-8 前端增加登录框

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,17 +22,75 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-<!--      <a-menu-item key="4">更多</a-menu-item>-->
+      <a class="login-menu" @click="showLoginModal">
+        <a-space>
+          <rocket-outlined/>
+          <span>登录</span>
+        </a-space>
+      </a>
     </a-menu>
+    <a-modal
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+        ok-text="出发!"
+        cancel-text="再想想~"
+    >
+      <!--    表单-->
+      <a-form :model="loginUser" :label-col="{span:6}" :wrapper-col="{span:18}">
+        <a-form-item label="用户名">
+          <a-input v-model:value="loginUser.loginName" />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
 <!--定义的组件-->
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 
 export default defineComponent({
   //组件名字
-  name: 'the-header'
+  name: 'the-header',
+  setup(){
+    const loginUser=ref({
+      loginName:"test",
+      password:"test"
+    });
+   // 显示框变量
+   const loginModalVisible=ref(false);
+   //loading状态
+   const loginModalLoading=ref(false);
+   //显示模块框
+   const showLoginModal = () => {
+     loginModalVisible.value=true;
+   };
+
+    //登录方法
+    const login = () => {
+      console.log("开始登录")
+    };
+   return{
+      loginModalVisible,
+      loginModalLoading,
+      loginUser,
+      showLoginModal,
+      login,
+   };
+  },
 });
 </script>
+
+<style>
+.login-menu{
+  position: relative;
+  left: 1300px;
+  color: white;
+  float: right;
+}
+</style>
