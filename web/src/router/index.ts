a8480a62991ba68b2088effc4a5b420bbcfbecd5
lzlonng <1661548820@qq.com>
2022-02-10 18:21:43 +0800

    9-12 增加路由登录拦截，未登录时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import Doc from '../views/doc.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -27,22 +29,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/ebook',
     name: 'AdminEbook',
-    component: AdminEbook
+    component: AdminEbook,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/category',
     name: 'AdminCategory',
-    component: AdminCategory
+    component: AdminCategory,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
-    component: AdminDoc
+    component: AdminDoc,
+    meta:{
+      loginRequire:true
+    }
   },
   {
     path: '/doc',
@@ -55,5 +69,25 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
+//路由拦截
+//每一次，to：新路由，from：旧路由，next：是一个方法
+router.beforeEach((to,from,next)=>{
+//  要不要对meta，loginRequire属性做监控拦截，item是路由整个变量
+  if (to.matched.some(function (item) {
+    console.log("是否需要登录校验:",item.meta.loginRequire);
+    return item.meta.loginRequire
+  })){
+    //拿出store中的user
+    const loginUser=store.state.user;
+    //如果为空就跳到首页
+    if (Tool.isEmpty(loginUser)){
+      console.log("用户未登录!");
+      next('/');
+    }else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 export default router
