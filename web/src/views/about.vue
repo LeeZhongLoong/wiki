39866162182aae60f6a5f10d340517c9e6324b7f
lzlonng <1661548820@qq.com>
2022-02-22 19:42:13 +0800

    11-9 添加内容到关于

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>关于我们</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="about">
+        <h2>我是ChampionLoong，一个小小学生</h2>
+        <div><b>我会更加努力学习~！</b></div>
+<!--        <div>-->
+<!--          <b>项目实战：-->
+<!--            <a href="https://coding.imooc.com/class/474.html" target="_blank">-->
+<!--              《SpringBoot知识体系+Vue3全家桶 前后端分离 实战WIKI知识库系统》-->
+<!--            </a>-->
+<!--          </b>-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <b>项目实战：-->
+<!--            <a href="https://coding.imooc.com/class/416.html" target="_blank">-->
+<!--              《Spring Cloud + Vue 前后端分离 开发企业级在线视频课程系统》-->
+<!--            </a>-->
+<!--          </b>-->
+<!--        </div>-->
+<!--        <div>-->
+<!--          <b>免费课程：-->
+<!--            <a href="https://www.imooc.com/learn/1160" target="_blank">-->
+<!--              《开发工具IDEA从入门到爱不释手》-->
+<!--            </a>-->
+<!--          </b>-->
+<!--        </div>-->
+        <br>
+        <div><b>QQ：1661548820，<span style="color: rgb(194, 79, 74)">更多学习内容</span>，加QQ，备注：小小知识库</b></div>
+        <div><b>微信号：龍</b></div>
+<!--        <div><b>个人全栈技术博客：-->
+<!--          <a href="http://www.jiawablog.com" target="_blank">甲蛙博客</a></b>-->
+<!--        </div>-->
+<!--        <div style="color: rgb(194, 79, 74)"><b>公众号：甲蛙全栈，扫码关注</b></div>-->
+<!--        <div>-->
+<!--          <img src="/image/jiawaquanzhan.jpg">-->
+<!--        </div>-->
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
